f3b60d5c52afa78095f0fcfb122b8bcb31705913
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-07-11 13:08:28 -0700

    Initial commit

---
+++ server/api/pg-resource.js
@@ -0,0 +1,180 @@
+function tagsQueryString(tags, itemid, result) {
+  /**
+   * Challenge:
+   * This function is more than a little complicated.
+   *  - Can you refactor it to be simpler / more readable?
+   */
+  const length = tags.length;
+  return length === 0
+    ? `${result};`
+    : tags.shift() &&
+    tagsQueryString(
+      tags,
+      itemid,
+      `${result}($${tags.length + 1}, ${itemid})${length === 1 ? '' : ','}`
+    );
+}
+
+module.exports = postgres => {
+  return {
+    async createUser({ fullname, email, password }) {
+      const newUserInsert = {
+        text: '', // @TODO: Authentication - Server
+        values: [fullname, email, password]
+      };
+      try {
+        const users = await postgres.query(newUserInsert);
+        return users.rows[0];
+      } catch (e) {
+        switch (true) {
+          case /users_fullname_key/.test(e.message):
+            throw 'An account with this username already exists.';
+          case /users_email_key/.test(e.message):
+            throw 'An account with this email already exists.';
+          default:
+            throw 'There was a problem creating your account.';
+        }
+      }
+    },
+    async getUserAndPasswordForVerification(email) {
+      const findUserQuery = {
+        text: '', // @TODO: Authentication - Server
+        values: [email]
+      };
+      try {
+        const users = await postgres.query(findUserQuery);
+        if (!users) throw 'User was not found.';
+        return users.rows[0];
+      } catch (e) {
+        throw 'User was not found.';
+      }
+    },
+    async getUserById(id) {
+      const findUserQuery = {
+        text: 'SELECT * FROM users WHERE id = $1 LIMIT 1',
+        values: [id]
+      };
+      try {
+        const users = await postgres.query(findUserQuery);
+        return users.rows[0];
+      } catch (e) {
+        throw "User not found";
+      }
+    },
+    async getItems(idToOmit) {
+      const items = await postgres.query({
+        text:  `SELECT * FROM items WHERE itemowner != $1`,
+        values: idToOmit ? [idToOmit] : []
+      });
+      return items.rows;
+    },
+    async getItemsForUser(id) {
+      const items = await postgres.query({
+        text: `SELECT * FROM items where id=$1`,
+        values: [id]
+      });
+      return items.rows;
+    },
+    async getBorrowedItemsForUser(id) {
+      const items = await postgres.query({
+        text: `SELECT *
+        FROM items
+        WHERE borrowerid =$1`,
+        values: [id]
+      });
+      return items.rows;
+    },
+    async getTags() {
+      const tags = await postgres.query({
+        text: `SELECT * FROM tags`
+      });
+      return tags.rows;
+    },
+    async getTagsForItem(id) {
+      const tagsQuery = {
+        text: ``, // @TODO: Advanced query Hint: use INNER JOIN
+        values: [id]
+      };
+
+      const tags = await postgres.query(tagsQuery);
+      return tags.rows;
+    },
+    async saveNewItem({ item, users }) {
+      /**
+       *  @TODO: Adding a New Item
+       *
+       *  Adding a new Item to Posgtres is the most advanced query.
+       *  It requires 3 separate INSERT statements.
+       *
+       *  All of the INSERT statements must:
+       *  1) Proceed in a specific order.
+       *  2) Succeed for the new Item to be considered added
+       *  3) If any of the INSERT queries fail, any successful INSERT
+       *     queries should be 'rolled back' to avoid 'orphan' data in the database.
+       *
+       *  To achieve #3 we'll ue something called a Postgres Transaction!
+       *  The code for the transaction has been provided for you, along with
+       *  helpful comments to help you get started.
+       *
+       *  Read the method and the comments carefully before you begin.
+       */
+
+      return new Promise((resolve, reject) => {
+        /**
+         * Begin transaction by opening a long-lived connection
+         * to a client from the client pool.
+         * - Read about transactions here: https://node-postgres.com/features/transactions
+         */
+        postgres.connect((err, client, done) => {
+          try {
+            // Begin postgres transaction
+            client.query('BEGIN', async err => {
+              const { title, description, tags } = item;
+
+              // Generate new Item query
+              // @TODO
+              // -------------------------------
+
+              // Insert new Item
+              // @TODO
+              // -------------------------------
+
+              // Generate tag relationships query (use the'tagsQueryString' helper function provided)
+              // @TODO
+              // -------------------------------
+
+              // Insert tags
+              // @TODO
+              // -------------------------------
+
+              // Commit the entire transaction!
+              client.query('COMMIT', err => {
+                if (err) {
+                  throw err;
+                }
+                // release the client back to the pool
+                done();
+                // Uncomment this resolve statement when you're ready!
+                // resolve(newItem.rows[0])
+                // -------------------------------
+              });
+            });
+          } catch (e) {
+            // Something went wrong
+            client.query('ROLLBACK', err => {
+              if (err) {
+                throw err;
+              }
+              // release the client back to the pool
+              done();
+            });
+            switch (true) {
+              default:
+                throw e;
+            }
+          }
+        });
+      });
+    }
+  };
+};
---
+++ server/api/schema.js
@@ -0,0 +1,67 @@
+const { gql } = require('apollo-server-express');
+
+
+module.exports = gql`
+
+  scalar Date
+  scalar Upload
+
+  type Item {
+    id: ID!
+    title: String!
+    imageurl: String
+    description: String! 
+    itemowner: User!
+    tags: [Tag]
+    created: Date!
+    borrower: User
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    fullname: String!
+    bio: String
+    items:[Item]
+    borrowed:[Item]
+  }
+
+  type Tag {
+    id: ID!
+    title: String!
+  }
+
+  type File {
+    id: ID!
+    filename: String!
+    mimetype: String!
+    encoding: String!
+    itemid: ID!
+  }
+
+  input AssignedTag {
+    id: ID!
+    title: String!
+  }
+
+  input AssignedBorrower {
+    id: ID!
+  }
+
+  input NewItemInput {
+    title: String!
+    description: String
+    tags: [AssignedTag]!
+  }
+
+  type Query {
+    users(id: ID!): User
+    viewer: User
+    items(filter: ID): [Item]
+    tags: [Tag]
+  }
+
+  type Mutation {
+    addItem(item: NewItemInput!, image: Upload): Item
+  }
+`;
